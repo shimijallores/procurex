35de83b6b8c480c5f6146b9b40c27ee71e837aec
zielww <118595018+zielww@users.noreply.github.com>
2026-02-07 12:20:33 +0800

    temporarily removed favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Procurex</title>
-    <link rel="icon" href="{{ asset('images/boxes.svg') }}" type="image/svg+xml">
+    {{-- <link rel="icon" href="{{ asset('images/boxes.svg') }}" type="image/svg+xml"> --}}
     @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
